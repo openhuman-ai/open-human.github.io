2d7ed6e1163ea728fe25631e75386aec4cee3e3f
Minh-Thanh Hoang <hmthanhgm@gmail.com>
2023-04-14 19:57:26 +0700

    update pinterest

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
       <meta name="facebook-domain-verification" content="nv49u10hdy8d6fj4s3vuipvsdjwpjc" />
+      <meta name="p:domain_verify" content="191947ba21d6a9680506234779e8e543"/>
       </Head>
       <body>
         <Main />
