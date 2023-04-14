c038f0b83d6f98710c7b9d2cb1ddf5127a007ad5
Minh-Thanh Hoang <hmthanhgm@gmail.com>
2023-04-14 19:20:12 +0700

    update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <meta name="facebook-domain-verification" content="nv49u10hdy8d6fj4s3vuipvsdjwpjc" />
+      </Head>
       <body>
         <Main />
         <NextScript />
